a8e6131497d66ab8d158425af1ec7b1b3443bb4f
Mark Fajet <mfajet@fiu.edu>
2017-09-16 16:04:21 -0400

    Added sorting

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import './App.css';
 import Book from './Components/Book.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={books: arr}
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value,books: sortByKey(arr,event.target.value)});
+  }
   render() {
     return (
       <div className="App">
@@ -14,7 +23,14 @@ class App extends Component {
         <p className="App-intro">
           We have books.
         </p>
-        {arr.map((book,i)=>
+        <select value={this.state.value} onChange={this.handleChange}>
+          <option value="title">Title</option>
+          <option value="author">Author</option>
+          <option value="price">Price</option>
+          <option value="rating">Rating</option>
+          <option value="releaseDate">Release Date</option>
+        </select>
+        {this.state.books.map((book,i)=>
           <Book key ={i}
                 title={book.title}
                 author={book.author}
@@ -29,48 +45,60 @@ class App extends Component {
     );
   }
 }
-
+function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key];
+        var y = b[key];
+        if(x<y){
+          return -1;
+        }else if(x>y){
+          return 1;
+        }else{
+          return 0
+        }
+    });
+}
 var arr=[
-          {title:"Mark's book",
-          author:"Mark",
+          {title:"Mark's book1",
+          author:"Mark5",
           imageSrc:"https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/20245452_10209642053625013_3811989541249208996_n.jpg?oh=88f001bcd0d2ca72b494e073a4d13e23&oe=5A413B57",
           genre:"good",
-          rating:"100%",
+          rating:10,
           price:"$10",
           releaseDate:"9/15/17"},
-          {title:"Mark's book",
-          author:"Mark",
+          {title:"Mark's book2",
+          author:"Mark4",
           imageSrc:"https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/20245452_10209642053625013_3811989541249208996_n.jpg?oh=88f001bcd0d2ca72b494e073a4d13e23&oe=5A413B57",
           genre:"good",
-          rating:"100%",
+          rating:7,
           price:"$10",
           releaseDate:"9/15/17"},
-          {title:"Mark's book",
-          author:"Mark",
+          {title:"Mark's book3",
+          author:"Mark3",
           imageSrc:"https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/20245452_10209642053625013_3811989541249208996_n.jpg?oh=88f001bcd0d2ca72b494e073a4d13e23&oe=5A413B57",
           genre:"good",
-          rating:"100%",
+          rating:5,
           price:"$10",
           releaseDate:"9/15/17"},
-          {title:"Mark's book",
-          author:"Mark",
+          {title:"Mark's book4",
+          author:"Mark2",
           imageSrc:"https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/20245452_10209642053625013_3811989541249208996_n.jpg?oh=88f001bcd0d2ca72b494e073a4d13e23&oe=5A413B57",
           genre:"good",
-          rating:"100%",
+          rating:6,
           price:"$10",
           releaseDate:"9/15/17"},
-          {title:"Mark's book",
-          author:"Mark",
+          {title:"Mark's book5",
+          author:"Mark1",
           imageSrc:"https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/20245452_10209642053625013_3811989541249208996_n.jpg?oh=88f001bcd0d2ca72b494e073a4d13e23&oe=5A413B57",
           genre:"good",
-          rating:"100%",
+          rating:3,
           price:"$10",
           releaseDate:"9/15/17"},
-          {title:"Mark's book",
-          author:"Mark",
+          {title:"Mark's book6",
+          author:"Mark0",
           imageSrc:"https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/20245452_10209642053625013_3811989541249208996_n.jpg?oh=88f001bcd0d2ca72b494e073a4d13e23&oe=5A413B57",
           genre:"good",
-          rating:"100%",
+          rating:1,
           price:"$10",
           releaseDate:"9/15/17"}
 ]
