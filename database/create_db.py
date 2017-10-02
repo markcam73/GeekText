ffd74ec81eec98d03c5fe0fbc115d1899b8bf905
Grace Deehl <gdeeh001@fiu.edu>
2017-10-02 16:33:13 -0400

    book sample check
    just checking how it looks

--- database/create_db.py
+++ database/create_db.py
@@ -10,7 +10,7 @@ with con:
     cur.execute("DROP TABLE IF EXISTS Books")
     cur.execute("DROP TABLE IF EXISTS ShoppingCart")
     cur.execute("CREATE TABLE Books(Id INT, Title TEXT, Author TEXT, ImageSrc TEXT, Genre TEXT, Rating INT, Price INT, ReleaseDate TEXT, CopiesSold INT)")
-    cur.execute("INSERT INTO Books VALUES(1,'Mark Fajets book', 'Mark Fajet', 'https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/20245452_10209642053625013_3811989541249208996_n.jpg?oh=88f001bcd0d2ca72b494e073a4d13e23&oe=5A413B57','Biography',5,5,'9/16/2017', 0)")
+    cur.execute("INSERT INTO Books VALUES(1,'The Hobbit', 'J. R. R. Tolkien', 'https://cdn.pastemagazine.com/www/system/images/photo_albums/hobbit-book-covers/large/photo_5653_0-7.jpg?1384968217','Sci-Fi & Fantasy',5,5,'9/16/2017', 0)")
     cur.execute("INSERT INTO Books VALUES(2,'Graces book', 'Grace', 'https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/20245452_10209642053625013_3811989541249208996_n.jpg?oh=88f001bcd0d2ca72b494e073a4d13e23&oe=5A413B57','Action',4,5,'9/15/2017', 0)")
     cur.execute("INSERT INTO Books VALUES(3,'Mark Camerons book', 'Mark Cameron', 'https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/20245452_10209642053625013_3811989541249208996_n.jpg?oh=88f001bcd0d2ca72b494e073a4d13e23&oe=5A413B57','Drama',3,5,'9/14/2017', 0)")
     cur.execute("INSERT INTO Books VALUES(4,'Mannys book', 'Manny', 'https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/20245452_10209642053625013_3811989541249208996_n.jpg?oh=88f001bcd0d2ca72b494e073a4d13e23&oe=5A413B57','Comedy',2,5,'9/13/2017', 0)")
