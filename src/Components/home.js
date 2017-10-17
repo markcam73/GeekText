a7add3d2410088bd2446ba8153077e8a70cc5339
mfajet <markfajet@gmail.com>
2017-10-17 19:56:30 -0400

    Removed unnecessary import

--- src/Components/home.js
+++ src/Components/home.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
-import Book from './Book';
 import API from '../API';
 
 class Home extends Component {
