c6fe0871c67188f7665d3437d31acc591358b002
Mark Cameron <markcam73@yahoo.com>
2017-10-27 09:49:58 -0400

    shopcart is now its own page with a route that unfortunately depends on location data for now

--- src/Components/BooksByAuthor.js
+++ src/Components/BooksByAuthor.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
 import BookList from './bookList';
-import Shopcart from './shopcart';
 
 class BookpageByAuthor extends Component{
     render(){
         return(
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-10">
-                        <BookList author={this.props.params.author}/>
-                    </div>
-                    <div className="col-md-2">
-                        <Shopcart />
-                    </div>
-                </div>
+            <div>
+                <BookList author={this.props.params.author}/>
             </div>
         );
     }
