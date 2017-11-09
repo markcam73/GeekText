050cf50c087c673308e7d87ed5eea033cccfa265
mfari <31676628+mfari@users.noreply.github.com>
2017-11-08 19:59:19 -0500

    Files related to comment system

---
+++ src/Components/comment.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+var Remarkable = require('remarkable'); 
+
+class Comment extends Component {
+rawMarkup() {
+	
+   var md = new Remarkable();
+   var rawMarkup = md.render(this.props.children.toString());
+   return { __html: rawMarkup };
+ }
+
+ render() {
+   return (
+     <div className="comment">
+       <h2 className="commentAuthor">
+         {this.props.author}
+       </h2>
+       <span dangerouslySetInnerHTML={this.rawMarkup()} />
+     </div>
+   );
+ }
+}
+export default Comment
