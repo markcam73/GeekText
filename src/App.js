a99a79f104065b65405ecaa5ce8975735a55a145
Mark Cameron <markcam73@yahoo.com>
2017-11-24 20:39:18 -0500

    changes to reflect cart

--- src/App.js
+++ src/App.js
@@ -11,14 +11,16 @@ class App extends Component {
         username:this.props.location.state.username,
         items: [],
         cartTotal:0,
-        headerCount:0
+        headerCount:0,
+        userID:-1
       }
     }else if(window.sessionStorage.token) {
       this.state={
         username:"",
         items: [],
         cartTotal:0,
-        headerCount:0
+        headerCount:0,
+        userID:-1
       }
       var payload = {
           token: window.sessionStorage.token
@@ -27,8 +29,6 @@ class App extends Component {
       API.postRequest(payload, '/profile/mine').then((jsonRes) => {
             if (jsonRes.status===200){
               _this.setState({username: jsonRes.username})
-            }else{
-
             }
       })
     }else{
@@ -40,12 +40,39 @@ class App extends Component {
     this.countTotal = this.countTotal.bind(this);
     this.handleTotal = this.handleTotal.bind(this);
     this.createCartElement = this.createCartElement.bind(this);
+    this.deleteCart = this.deleteCart.bind(this);
   }
 
-  componentDidMount(){
+  componentWillMount(){
+    if(window.sessionStorage.token) {
+      var payload = {
+        token: window.sessionStorage.token
+      };
+      var _this = this;
+      API.postRequest(payload, '/shopcart').then((jsonRes) => {
+        console.log(jsonRes);
+        if (jsonRes.status===200){
+          _this.setState({items:jsonRes.items, cartTotal: jsonRes.cartTotal, headerCount: jsonRes.headerCount, userID: jsonRes.userID})
+          if(this.state.items.length > 0){
+            alert("you have saved items in your cart!");
+            console.log(this.state);
+          }                
+        }
+      })
+    }
+    console.log(this.state)
     PubSub.subscribe('cart.added', this.handleAdd)
     PubSub.subscribe('cart.edited', this.handleHeader)
     PubSub.subscribe('change.price',this.handleTotal)
+    PubSub.subscribe('cart.wiped', this.deleteCart)
+  }
+
+  deleteCart(e){
+    this.setState({
+      items: [],
+      cartTotal:0,
+      headerCount:0,
+    })
   }
   createCartElement(item){
     var e = {}
@@ -136,7 +163,7 @@ class App extends Component {
           <button onClick={()=>API.changePath("/home/",{username:this.state.username})}style = {{height: '24px'}}><small>Best Sellers</small></button>
           <button onClick={()=>API.changePath("/books/",{username:this.state.username})}style = {{height: '24px'}}><small>Browse</small></button>
           <button onClick={()=>API.changePath("/profile",{username:this.state.username})}style = {{height: '24px'}}><small>Account</small></button>
-          <button onClick={()=>API.changePath("/shopcart/", {username:this.state.username, books:this.state.items, cartTotal: this.state.cartTotal, header: this.state.headerCount})} style = {{float: 'right', height: '24px'}}> <img src = './cart.png' alt="cart" style = {{float: 'center', width: '32px', height: '20px'}}/>
+          <button onClick={()=>API.changePath("/shopcart", {username:this.state.username, books:this.state.items, cartTotal: this.state.cartTotal, header: this.state.headerCount, userID: this.state.userID})} style = {{float: 'right', height: '24px'}}> <img src = './cart.png' alt="cart" style = {{float: 'center', width: '32px', height: '20px'}}/>
             {this.state.headerCount > 0 ? content: empty}
           </button>
         </div>
